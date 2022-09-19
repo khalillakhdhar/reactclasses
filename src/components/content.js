50c3e2e798ee39251eb8f7872c748373452627ed
khalil <khalillakhdharatc@gmail.com>
2022-09-19 11:15:25 +0100

    demo

---
+++ src/components/content.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Content extends React.Component {
+    render(){
+        return (
+            <div>
+React est une bibliothéque javascript one page RealTime <strong>facile et efficace</strong>
+            </div>
+            
+            )}
+
+}
+export default Content;
---
+++ src/components/headers.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Headers  extends React.Component {
+render()
+{
+    return(
+        <>
+<h1> Bienvenu à React JS</h1>
+<h2>C'est quoi React?</h2>
+
+        </>
+        );
+}
+
+
+}
+export default Headers;
